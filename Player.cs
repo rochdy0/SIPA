8e51413327ccc8463336a4c235a64bb9696da7ed
rochdy0 <rochdy0@gmail.com>
2023-12-28 00:23:20 +0100

    First commit

---
+++ Player.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player{
+
+    public CardStack hand;
+    private string name;
+
+    public Player(string name){
+        this.name = name;
+        hand = new CardStack();
+    }
+
+    public string getNom(){
+        return this.name;
+    }
+
+    public override string ToString(){
+        return "je m'appelle : "+this.name+" ma main est : "+hand.ToString();
+    }
+
+}
+
+
